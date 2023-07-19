fbcb251a6a373d1f26d757e52d79971322bdc5ef
Edgar Juan Guercio <jguercio@live.com.ar>
2023-07-19 19:26:10 -0300

    conflicto index resuelto

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,30 @@
+const multer = require("multer")
 const { Router } = require('express');
 const router = Router();
 const { getAllProductsHandler,
-    getCategoriesHandler, createProductHandler, deleteProductHandler } = require('../handlers/');
+    getCategoriesHandler,
+    createProductHandler, deleteProductHandler } = require('../handlers/');
+
+const storage = multer.diskStorage({
+
+    destination: (req, res, cb) => {
+
+        cb(null, "./src/assets/products")
+
+    },
+    filename: (req, file, cb) => {
+
+        cb(null, "product-" + Date.now() + "-" + file.originalname)
+    }
+})
+
+const productsUploads = multer({ storage })
+
 
 router.get('/products', getAllProductsHandler);
 router.get('/categories', getCategoriesHandler);
-router.post('/products', createProductHandler);
 router.delete("/products/:id", deleteProductHandler);
+//image es el nombre del field html por el cual se recibe el archivo
+router.post('/products', [productsUploads.single("image")], createProductHandler);
+
 module.exports = router;
