a5610a1e296e6f3e9aec9427f5bfd36b3852f91f
charliejp21 <charliejp21@gmail.com>
2023-07-18 22:40:26 -0600

    Post de proucto con imagen

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,29 @@
+const multer = require("multer")
 const { Router } = require('express');
 const router = Router();
 const { getAllProductsHandler,
-    getCategoriesHandler, createProductHandler } = require('../handlers/');
+    getCategoriesHandler,
+    createProductHandler } = require('../handlers/');
+
+const storage = multer.diskStorage({
+
+    destination: (req, res, cb) => {
+
+        cb(null, "./src/assets/products")
+
+    },
+    filename: (req, file, cb) => {
+
+        cb(null, "product-" + Date.now() + "-" + file.originalname)
+    }
+})
+
+const productsUploads = multer({ storage })
+
 
 router.get('/products', getAllProductsHandler);
 router.get('/categories', getCategoriesHandler);
-router.post('/products', createProductHandler);
+//fileProduct es el nombre del field html por el cual se recibe el archivo
+router.post('/products', [productsUploads.single("image")], createProductHandler);
 
 module.exports = router;
