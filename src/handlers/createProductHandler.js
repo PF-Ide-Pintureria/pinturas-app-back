d5e5272211945040e4dc83aea2af1cf998ba10df
Jamer Rebolledo <jamerrq@gmail.com>
2023-07-20 15:44:31 -0500

    adding image processing into try/catch block

--- src/handlers/createProductHandler.js
+++ src/handlers/createProductHandler.js
@@ -1,39 +1,40 @@
 const { createProductController } = require('../controllers');
 const fs = require("fs");
 
+
 const createProductHandler = async (req, res) => {
 
-    if (!req.file) return res.status(400)
-        .send("No se ha proporcinado la imagen de producto");
-
-    //Obtener nombre de la imagen;
-    let imgProduct = req.file.originalname;
-
-    //Sacar la extensión;
-    let extension = imgProduct.split(".").at(-1);
-
-    //Comprobar extension;
-    if (extension !== "png" && extension !== "jpg" && extension !== "jpeg"
-        && extension !== "gif" && extension !== "webp") {
-
-        //Si no es la extensión correcta eliminar el archivo;
-        const filePath = req.file.path;
-
-        fs.unlinkSync(filePath);
-
-        return res.status(400).json({
-            status: "error",
-            mensaje: "Por favor sube un formato válido de imagen"
-        });
-
-    };
-
-    // si la imagen es correcta agregar a la bd;
-    const saveImage = req.file.filename;
-
-    req.body.image = saveImage;
-
     try {
+
+        if (!req.file) return res.status(400)
+            .send("No se ha proporcinado la imagen de producto");
+
+        //Obtener nombre de la imagen;
+        let imgProduct = req.file.originalname;
+
+        //Sacar la extensión;
+        let extension = imgProduct.split(".").at(-1);
+
+        //Comprobar extension;
+        if (extension !== "png" && extension !== "jpg" && extension !== "jpeg"
+            && extension !== "gif" && extension !== "webp") {
+
+            //Si no es la extensión correcta eliminar el archivo;
+            const filePath = req.file.path;
+
+            fs.unlinkSync(filePath);
+
+            return res.status(400).json({
+                status: "error",
+                mensaje: "Por favor sube un formato válido de imagen"
+            });
+
+        };
+
+        // si la imagen es correcta agregar a la bd;
+        const saveImage = req.file.filename;
+
+        req.body.image = saveImage;
         const [product] = await createProductController(req.body);
         return res.status(201).json({
 
@@ -43,9 +44,11 @@ const createProductHandler = async (req, res) => {
             imagen: req.file
 
         });
+
     } catch (error) {
         return res.status(500).json({ error: error.message });
     };
+
 };
 
 module.exports = createProductHandler;
