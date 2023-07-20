32efa80946936f19fcab343766f3610ea8d7f5ac
Jamer Rebolledo <jamerrq@gmail.com>
2023-07-20 17:06:25 -0500

    added middlewares for rate limits

---
+++ src/middlewares/rateLimiter.js
@@ -0,0 +1,9 @@
+const RateLimiter = require('express-rate-limit');
+
+const rateLimiter = new RateLimiter({
+    windowMs: 1000 * 60 * 60,
+    max: 100,
+    message: 'Excediste el número de peticiones por hora'
+});
+
+module.exports = rateLimiter;
