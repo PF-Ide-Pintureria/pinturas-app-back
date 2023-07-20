7fd0a9eb465cffac80ddf81a4cf87b9175222548
Jamer Rebolledo <jamerrq@gmail.com>
2023-07-20 17:46:58 -0500

    hotfixes, cors y rate limiter

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -1,6 +1,6 @@
 const RateLimiter = require('express-rate-limit');
 
-const rateLimiter = new RateLimiter({
+const rateLimiter = RateLimiter({
     windowMs: 1000 * 60 * 60,
     max: 100,
     message: 'Excediste el número de peticiones por hora'
