9fb100a2cf915bdc9a1baf142328ea4875888131
Jamer Rebolledo <jamerrq@gmail.com>
2023-07-21 21:09:00 -0500

    listos los filtros desde sequelize

--- src/controllers/filterAndOrderProductsController.js
+++ src/controllers/filterAndOrderProductsController.js
@@ -3,122 +3,110 @@ const { Products } = require('../db.js');
 
 const PRODUCTS_PER_PAGE = 12;
 
-  const filterAndOrderProductsController = async (
+const filterAndOrderProductsController = async ({
+    name, category,
+    lowPrice, highPrice,
+    minRating, maxRating,
+    minStock, maxStock,
+    page, color,
+    active, limit,
+    sortBy, orderBy,
+}) => {
 
-  category,
-  lowPrice, highPrice,
-  minRating, maxRating,
-  minStock, maxStock,
-  page,
-  color,
-  active,
-  limit,
-  sortBy,
-  orderBy,
+    const pageSet = page || 1; // Página actual, por defecto 1
 
-  ) => {
+    const offset = (pageSet - 1) * PRODUCTS_PER_PAGE;
 
-  const pageSet = page || 1; // Página actual, por defecto 1
+    // Construir la condición de búsqueda para los términos proporcionados
+    let searchCondition = {
+        // Búsqueda por categoría si se proporciona una categoría específica
+        ...(category && { category }),
 
-  const offset = (pageSet - 1) * PRODUCTS_PER_PAGE;
+        // Búsqueda por color
+        ...(color && { color }),
 
-  // Construir la condición de búsqueda para los términos proporcionados
-  let searchCondition = {
-    // Búsqueda por categoría si se proporciona una categoría específica
-    ...(category && { 
+        // Búsqueda por activos
+        ...(active && { active }),
 
-        category: category }),
-        
-    /* // Búsqueda por color
-    ...(color && {
-        
-        color: color }),
+        // Búsqueda por rango de precios
+        ...((lowPrice && highPrice) ?
+            {
+                price: {
+                    [Op.between]: [lowPrice, highPrice],
+                },
+            } : (lowPrice ? {
+                price: {
+                    [Op.gte]: lowPrice,
+                },
+            } : (highPrice ? {
+                price: {
+                    [Op.lte]: highPrice,
+                },
+            } : {}))),
 
-    // Búsqueda por activos
-    ...(active  && { 
-        
-        active: active }),
+        // Búsqueda por rating
+        ...((minRating && maxRating) ?
+            {
+                rating: {
+                    [Op.between]: [minRating, maxRating],
+                },
+            } : (minRating ? {
+                rating: {
+                    [Op.gte]: minRating,
+                },
+            } : (maxRating ? {
+                rating: {
+                    [Op.lte]: maxRating,
+                },
+            } : {}))),
 
-    // Búsqueda por rango de precios
-    ...(lowPrice && maxPrice && {
-
-        price: {
-            [Op.between]: [lowPrice, highPrice],
-        },
-
-        }),
-    ...(lowPrice && !highPrice && {
-
-        price: {
-            [Op.gte]: lowPrice,
-        },
-        }),
-        ...(highPrice && !lowPrice && {
-        precio: {
-            [Op.lte]: highPrice,
-        },
-        }),
-    // Búsqueda por rating
-    ...(minRating && maxRating && {
-
-        rating: {
-            [Op.between]: [minRating, maxRating],
-        },
-
-        }),
-    ...(minRating && !maxRating && {
-
-        rating: {
-            [Op.gte]: minRating,
-        },
-        }),
-        ...(maxRating && !minRating && {
-        prating: {
-            [Op.lte]: maxRating,
-        },
-        }),  
         // Búsqueda por stock
-    ...(minStock && maxStock && {
-
-        stock: {
-            [Op.between]: [minStock, maxStock],
-        },
-
-        }),
-    ...(minStock && !maxStock && {
-
-        stock: {
-            [Op.gte]: minStock,
-        },
-        }),
-        ...(maxStock && !minStock && {
-        stock: {
-            [Op.lte]: maxStock,
-        },
-        }),   */
+        ...((minStock && maxStock) ?
+            {
+                stock: {
+                    [Op.between]: [minStock, maxStock],
+                },
+            } : (minStock ? {
+                stock: {
+                    [Op.gte]: minStock,
+                },
+            } : (maxStock ? {
+                stock: {
+                    [Op.lte]: maxStock,
+                },
+            } : {}))),
 
     };
-   
-    let products = await Products.findAndCountAll({
-      where: searchCondition,
-      //limit: PRODUCTS_PER_PAGE,
-      offset: offset,
 
+    let products = await Products.findAndCountAll({
+        where: searchCondition,
+        limit: limit || PRODUCTS_PER_PAGE,
+        offset: offset,
+        order: [
+            // Ordenar por precio
+            ...(sortBy === 'price' ? [['price', orderBy || 'ASC']] : []),
+            // Ordenar por rating
+            ...(sortBy === 'rating' ? [['rating', orderBy || 'ASC']] : []),
+            // Ordenar por stock
+            ...(sortBy === 'stock' ? [['stock', orderBy || 'ASC']] : []),
+            // Ordenar por nombre
+            ...(sortBy === 'name' ? [['name', orderBy || 'ASC']] : []),
+        ],
     });
-   
+
     // Consulta adicional para obtener la cantidad total de resultados
-    const totalResults = await Products.count({ where: searchCondition });
+    const totalResults = products.count;
 
     // Calcular la cantidad total de páginas
     const totalPages = Math.ceil(totalResults / PRODUCTS_PER_PAGE);
 
-    if (products.length === 0) {
-      throw new Error('No se encontraron resultados. Prueba de otra manera.');
-    }
+    if (!products.rows.lengths) {
+        throw new Error('No se encontraron resultados. Prueba de otra manera.');
+    };
 
     return {
-      results: products,
-      pages: totalPages,
+        results: products,
+        pages: totalPages,
     };
 
 };
--- src/handlers/getAllProductsHandler.js
+++ src/handlers/getAllProductsHandler.js
@@ -3,8 +3,7 @@ const { filterAndOrderProductsController } = require('../controllers');
 
 const getAllProductsHandler = async (req, res) => {
     try {
-        // parámetros de búsqueda: nombre, precio (límite inferior y superior),
-        // categoría
+
         const {
             name, category,
             lowPrice, highPrice,
@@ -14,7 +13,8 @@ const getAllProductsHandler = async (req, res) => {
             color, active,
             sortBy, orderBy,
         } = req.query;
-        const filteredProducts = await filterAndOrderProductsController(
+
+        const filteredProducts = await filterAndOrderProductsController({
             name, category,
             lowPrice, highPrice,
             minRating, maxRating,
@@ -22,8 +22,10 @@ const getAllProductsHandler = async (req, res) => {
             limit, page,
             color, active,
             sortBy, orderBy,
-        );
+        });
+
         return res.status(200).json(filteredProducts);
+
     } catch (error) {
         return res.status(500).json({ error: error.message });
     };
