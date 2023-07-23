7cc745c7fb0c8e88e7d604d08b4db8007a1a1069
charliejp21 <charliejp21@gmail.com>
2023-07-23 16:08:37 -0600

    Corrección de cloudinary en edit product

--- src/handlers/editProductHandler.js
+++ src/handlers/editProductHandler.js
@@ -1,21 +1,30 @@
 const { editProductController } = require('../controllers/');
-const { uploadImage } = require('../services');
+const uploadImage = require('../services/cloudinary');
 
 
 const editProductHandler = async (req, res) => {
 
     try {
+
         const { id } = req.params;
-        const secure_url = await uploadImage(req.file.path);
-        req.body.image = secure_url;
+
+        if(req.file){
+
+            const secure_url = await uploadImage(req.file);
+
+            req.body.image = secure_url;    
+            
+        }
+
         const product = await editProductController(id, req.body);
 
-        return res.status(200).json({
+        return res.status(201).json({
             status: "success",
             message: "Producto editado exitosamente",
-            product,
+            product: product
         });
 
+    
     } catch (error) {
         return res.status(500).json({ error: error.message });
     };
