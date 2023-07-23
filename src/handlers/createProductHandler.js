5caf229e1f60c5234a83b7a9b5012844b2268050
charliejp21 <charliejp21@gmail.com>
2023-07-23 03:59:02 -0600

    Cloudinary en create product

--- src/handlers/createProductHandler.js
+++ src/handlers/createProductHandler.js
@@ -1,8 +1,15 @@
 const { createProductController } = require('../controllers');
 const fs = require("fs");
 const sanitize = require("sanitize-filename");
+const cloudinary = require("cloudinary").v2;
+require('dotenv').config();
+const { CLOUD_NAME, CLOUDINARY_KEY, CLOUDINARY_SECRET } = process.env;
 
-
+cloudinary.config({ 
+    cloud_name: CLOUD_NAME, 
+    api_key: CLOUDINARY_KEY, 
+    api_secret: CLOUDINARY_SECRET 
+  });
 const createProductHandler = async (req, res) => {
 
     try {
@@ -24,15 +31,15 @@ const createProductHandler = async (req, res) => {
             // Validar si el path es seguro;
             const safePath = sanitize(filePath, { replacement: "_" });
             if (safePath !== filePath) {
-                throw new Error("El path no es seguro");
+
+                fs.unlinkSync(filePath);
+
+                return res.status(400).json({
+                    status: "error",
+                    mensaje: "Por favor sube un formato válido de imagen"
+                });
+
             };
-
-            fs.unlinkSync(filePath);
-
-            return res.status(400).json({
-                status: "error",
-                mensaje: "Por favor sube un formato válido de imagen"
-            });
 
         };
 
@@ -40,16 +47,46 @@ const createProductHandler = async (req, res) => {
         const saveImage = req.file ? req.file.path : defaultImagePath;
 
         req.body.image = saveImage;
-        const [product] = await createProductController(req.body);
-        return res.status(201).json({
+        
+        await cloudinary.uploader.upload(`${req.file.path}`,
+        { public_id: `${req.file.filename}` }, 
+        async function(error, result) {
 
-            status: "success",
-            message: "Producto publicado exitosamente",
-            product
+            if(result){
+
+                req.body.image = result.url;
+
+                const [product] = await createProductController(req.body);
+
+                return res.status(201).json({
+
+                    status: "success",
+                    message: "Producto publicado exitosamente",
+                    product
+                });
+
+            }
+            
+            if(error){
+
+                const [product] = await createProductController(req.body);
+
+                return res.status(201).json({
+
+                    status: "success",
+                    message: "Producto publicado exitosamente",
+                    product
+                });
+
+
+            }
+        
         });
 
     } catch (error) {
+
         return res.status(500).json({ error: error.message });
+
     };
 
 };
