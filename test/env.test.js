b87d9327728d4f67c1a8db01fc4c702fecd3cf60
Jamer Rebolledo <jamerrq@gmail.com>
2023-07-25 00:02:33 -0500

    created test for .env file

---
+++ test/env.test.js
@@ -0,0 +1,69 @@
+require('dotenv').config();
+const { expect } = require('chai');
+
+// Verifica que las variables de entorno estén definidas
+describe('ENVIRONMENT VARIABLES', () => {
+
+    // Verifica que la variables de entorno para la
+    // conexión a la base de datos estén definidas
+    describe('Database keys', () => {
+
+        it('Should have a DB_NAME key', () => {
+            expect(process.env.DB_NAME).to.not.be.undefined;
+        });
+
+        it('Should have a DB_HOST key', () => {
+            expect(process.env.DB_HOST).to.not.be.undefined;
+        });
+
+        it('Should have a DB_USER key', () => {
+            expect(process.env.DB_USER).to.not.be.undefined;
+        });
+
+        it('Should have a DB_PASSWORD key', () => {
+            expect(process.env.DB_PASS).to.not.be.undefined;
+        });
+
+    });
+
+    // Verifica que la variables de entorno para la
+    // conexión a Cloudinary estén definidas
+    describe('Cloudinary keys', () => {
+
+        it('Should have a CLOUD_NAME key', () => {
+            expect(process.env.CLOUD_NAME).to.not.be.undefined;
+        });
+
+        it('Should have a CLOUDINARY_KEY key', () => {
+            expect(process.env.CLOUDINARY_KEY).to.not.be.undefined;
+        });
+
+        it('Should have a CLOUDINARY_SECRET key', () => {
+            expect(process.env.CLOUDINARY_SECRET).to.not.be.undefined;
+        });
+
+    });
+
+    // Verifica que la variables de entorno para la
+    // conexión a DB de testing estén definidas
+    describe('Testing database keys', () => {
+
+        it('Should have a DB_TEST_NAME key', () => {
+            expect(process.env.DB_TEST_NAME).to.not.be.undefined;
+        });
+
+        it('Should have a DB_TEST_HOST key', () => {
+            expect(process.env.DB_TEST_HOST).to.not.be.undefined;
+        });
+
+        it('Should have a DB_TEST_USER key', () => {
+            expect(process.env.DB_TEST_USER).to.not.be.undefined;
+        });
+
+        it('Should have a DB_TEST_PASSWORD key', () => {
+            expect(process.env.DB_TEST_PASS).to.not.be.undefined;
+        });
+
+    });
+
+});
