2451530f12a445eb958cd4cc6debc4b28d2ac68a
Jamer Rebolledo <jamerrq@gmail.com>
2023-07-28 11:50:19 -0500

    [HOTFIX] nodemailer serice settings

--- src/services/nodemailer.js
+++ src/services/nodemailer.js
@@ -4,10 +4,14 @@ const { SENDER_MAIL: email, SENDER_PASS: password } = process.env;
 
 const transporter = nodemailer.createTransport({
     service: "gmail",
+    secure: false,
     auth: {
         user: email,
         pass: password,
     },
+    tls: {
+        rejectUnauthorized: false,
+    }
 });
 
 
