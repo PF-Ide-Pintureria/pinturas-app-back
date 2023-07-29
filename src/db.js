b1ba0cdd0002dba1ccf960abb0944368c766f8e8
Jamer Rebolledo <jamerrq@gmail.com>
2023-07-29 12:20:57 -0500

    [ TESTS ] modularizate db to test multiple instances

--- src/db.js
+++ src/db.js
@@ -4,14 +4,15 @@ let { DB_USER, DB_PASS, DB_HOST, DB_NAME } = process.env;
 const fs = require('fs');
 const path = require('path');
 
-let sequelize;
+
+let sequelizeInstance = null;
 const { NODE_ENV } = process.env;
 if (NODE_ENV === 'test') {
     const {
         DB_TEST_USER, DB_TEST_PASS,
         DB_TEST_HOST, DB_TEST_NAME
     } = process.env;
-    createDBInstance(DB_TEST_USER, DB_TEST_PASS,
+    sequelizeInstance = createDBInstance(DB_TEST_USER, DB_TEST_PASS,
         DB_TEST_HOST, DB_TEST_NAME);
     console.log('[DB] TESTING ENVIRONMENT');
 } else if (NODE_ENV === 'local') {
@@ -19,11 +20,11 @@ if (NODE_ENV === 'test') {
         DB_LOCAL_USER, DB_LOCAL_PASS,
         DB_LOCAL_HOST, DB_LOCAL_NAME
     } = process.env;
-    createDBInstance(DB_LOCAL_USER, DB_LOCAL_PASS,
+    sequelizeInstance = createDBInstance(DB_LOCAL_USER, DB_LOCAL_PASS,
         DB_LOCAL_HOST, DB_LOCAL_NAME);
     console.log('[DB] LOCAL ENVIRONMENT');
 } else {
-    createDBInstance();
+    sequelizeInstance = createDBInstance();
     console.log('[DB] MAIN ENVIRONMENT');
 }
 
@@ -31,7 +32,9 @@ function createDBInstance(user = DB_USER, pass = DB_PASS,
     host = DB_HOST, name = DB_NAME) {
     let dataBaseUrl = `postgres://${user}:${pass}@${host}/${name}`;
     try {
-        sequelize = new Sequelize(dataBaseUrl, {
+
+        // Creamos la instancia de Sequelize
+        const sequelizeInstance = new Sequelize(dataBaseUrl, {
             // set to console.log to see the raw SQL queries
             logging: false,
             // lets Sequelize know we can use pg-native for ~30% more speed
@@ -41,6 +44,14 @@ function createDBInstance(user = DB_USER, pass = DB_PASS,
                 require: true,
             }
         });
+        // Injectamos los modelos a la instancia de sequelize
+        injectModels(sequelizeInstance);
+
+        // Injectamos las relaciones
+        const { createRelations } = require('./dbRelations');
+        createRelations(sequelizeInstance);
+
+        return sequelizeInstance;
     }
     catch (error) {
         console.log('Error al cargar la base de datos :(');
@@ -49,49 +60,37 @@ function createDBInstance(user = DB_USER, pass = DB_PASS,
 }
 
 
-const basename = path.basename(__filename);
-const modelDefiners = [];
-// Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al
-// arreglo modelDefiners
-fs.readdirSync(path.join(__dirname, '/models'))
-    .filter((file) =>
-        (file.indexOf('.') !== 0) &&
-        (file !== basename) &&
-        (file.slice(-3) === '.js')
-    ).forEach((file) => {
-        modelDefiners.push(require(path.join(__dirname, '/models', file)));
-    });
+function injectModels(sequelizeInstance) {
 
-// Injectamos la conexion (sequelize) a todos los modelos
-modelDefiners.forEach(model => model(sequelize));
-// Capitalizamos los nombres de los modelos ie: product => Product
-let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase()
-    + entry[0].slice(1), entry[1]]);
-sequelize.models = Object.fromEntries(capsEntries);
+    const basename = path.basename(__filename);
+    const modelDefiners = [];
+    // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al
+    // arreglo modelDefiners
+    fs.readdirSync(path.join(__dirname, '/models'))
+        .filter((file) =>
+            (file.indexOf('.') !== 0) &&
+            (file !== basename) &&
+            (file.slice(-3) === '.js')
+        ).forEach((file) => {
+            modelDefiners.push(require(path.join(__dirname, '/models', file)));
+        });
 
-// En sequelize.models están todos los modelos importados como propiedades
-// Para relacionarlos hacemos un destructuring
-const { Blogs, Products, Reviews, Users } = sequelize.models;
+    // Injectamos la conexion (sequelize) a todos los modelos
+    modelDefiners.forEach(model => model(sequelizeInstance));
+    // Capitalizamos los nombres de los modelos ie: product => Product
+    let entries = Object.entries(sequelizeInstance.models);
+    let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase()
+        + entry[0].slice(1), entry[1]]);
+    sequelizeInstance.models = Object.fromEntries(capsEntries);
 
-// Aca vendrian las relaciones:
-//FAVORITOS
-// un usuario puede tener muchos productos favoritos y un producto puede ser favorito para muchos usuarios.
-Users.belongsToMany(Products, { through: "favorite_products" });
-Products.belongsToMany(Users, { through: "favorite_products" });
-//BLOGS
-//un usuario puede tener varios blogs, pero cada blog pertenece a un único usuario
-Users.hasMany(Blogs);
-Blogs.belongsTo(Users);
-//REVIEWS
-//un usuario puede realizar muchas reseñas pero cada reseña esta asociada a un unico producto
-Users.hasMany(Reviews);
-Reviews.belongsTo(Users);
-
-Products.hasMany(Reviews);
-Reviews.belongsTo(Products);
+}
 
 module.exports = {
-    ...sequelize.models,
-    conn: sequelize,
+    // De esta manera podemos acceder a los modelos por el nombre
+    // de la entidad e.g. User, Product, etc.
+    ...sequelizeInstance.models,
+    // Para tener acceso a la conexión desde cualquier parte
+    conn: sequelizeInstance,
+    // Para poder testear las diferentes conexiones
+    createDBInstance,
 };
