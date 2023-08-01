de761bb406bec70f636c7344544d2f5b3d38faed
Edgar Juan Guercio <jguercio@live.com.ar>
2023-08-01 02:19:52 -0300

    ruta add Favorite Product

--- src/controllers/Favorites/addFavoriteController.js
+++ src/controllers/Favorites/addFavoriteController.js
@@ -1,5 +1,15 @@
-const addFavoriteController = () => {
-    return "controller ADD FAV";
+const { Users, Products } = require("../../db");
+
+const addFavoriteController = async (idUser, idProduct) => {
+    const user = await Users.findByPk(idUser);
+    if (!user) throw Error("Error: Usuario no encontrado");
+
+    const productFavorite = await Products.findByPk(idProduct);
+    if (!productFavorite) throw Error("Error: Producto no econtrado");
+
+    await user.addProduct(productFavorite);
+
+    return await user.getProducts();
 };
 
 module.exports = addFavoriteController;
