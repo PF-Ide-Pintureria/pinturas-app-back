a2d2faa21668b96c92be130819287f2c6dc0b651
charliejp21 <charliejp21@gmail.com>
2023-08-02 19:05:15 -0600

    Authzero terminado con repo actualizado

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { NODE_PORT } = ENV || 3001;
 
 
 // Syncing all the models at once.
-conn.sync({ alter: false }).then(() => {
+conn.sync({ alter: true }).then(() => {
     server.listen(NODE_PORT, "0.0.0.0", async () => {
         // eslint-disable-next-line no-console
         console.log(`Server listening on port ${NODE_PORT}`);
