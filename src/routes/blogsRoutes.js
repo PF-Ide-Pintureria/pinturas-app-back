a131d08874c33822fde95236657c97053b886d68
BachiRomero <romerobasilio@hotmail.com.ar>
2023-08-03 22:27:14 -0300

    creando ruta en el flujo de blogs

---
+++ src/routes/blogsRoutes.js
@@ -0,0 +1,18 @@
+const { Router } = require("express");
+const { BlogsHandlers } = require("../handlers/");
+const { requiresAuth } = require("express-openid-connect");
+const auth = require("../middlewares/auth");
+
+const router = Router();
+
+// 1. POST /blogs/create
+router.post("/create", BlogsHandlers.createBlogs);
+
+// 2. GET /blogs
+router.get("/", BlogsHandlers.getBlogs);
+
+//3. DELETE /blogs/:id
+router.delete("/:id", BlogsHandlers.deleteBlogs);
+
+//4. PUT /blogs/:id
+router.put("/:id", BlogsHandlers.putBlogs);
