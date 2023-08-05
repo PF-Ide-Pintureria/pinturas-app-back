669827bf280b5d2b6695a215ffe762c0c08ddcec
Edgar Juan Guercio <jguercio@live.com.ar>
2023-08-05 15:55:38 -0300

    otro cambio

--- src/controllers/Blogs/createBlogsController.js
+++ src/controllers/Blogs/createBlogsController.js
@@ -1,7 +1,10 @@
-const { Blogs } = require("../../db.js");
+const { Blogs, Users } = require("../../db.js");
 
 const createBlogsController = async (blog, userId) => {
-    console.log(userId);
+    const userPost = Users.findByPk(userId);
+    if (!userPost) throw Error("No se encontro el usuario");
+
+    //console.log(userId);
     return Blogs.findOrCreate({
         where: {
             ...blog,
