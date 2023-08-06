96a55817bfc516dc63397eae6b7968e47c4f6db0
Jamer Rebolledo <jamerrq@gmail.com>
2023-08-06 17:16:37 -0500

    [ ORDERS WEBHOOK ] commit de prueba para activar la PR, no aprobar ni mergear por favor

--- src/controllers/Orders/webHookController.js
+++ src/controllers/Orders/webHookController.js
@@ -1,4 +1,4 @@
-const { Orders } = require("../../db");
+const { Orders, Products } = require("../../db");
 
 
 const webHookController = async ({ idOrder, state }) => {
@@ -7,6 +7,14 @@ const webHookController = async ({ idOrder, state }) => {
     if (!order) throw Error("Error: No se encontro la orden");
     order.state = state;
     await order.save();
+    const products = order.products;
+    for (let i = 0; i < products.length; i++) {
+        const parsedProduct = JSON.parse(products[i]);
+        const product = await Products.findByPk(parsedProduct[i].id);
+        if (!product) throw Error("Error: No se encontro el producto");
+        product.stock = Math.max(product.stock - products[i].orderline.quantity, 0);
+        await product.save();
+    }
     return order;
 
 };
