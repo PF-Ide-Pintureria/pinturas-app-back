e6c9019aeb2e64816b332447acd69ad134c8c43a
Jamer Rebolledo <jamerrq@gmail.com>
2023-08-07 15:50:03 -0500

    [HOTFIX] added query options for getOrders (idUser, state, idOrder)

--- src/handlers/Orders/getOrdersHandler.js
+++ src/handlers/Orders/getOrdersHandler.js
@@ -5,7 +5,7 @@ const { getOrders } = OrdersControllers;
 const getOrdersHandler = async (req, res) => {
 
     try {
-        const { idUser, state, idOrder } = req.body;
+        const { idUser, state, idOrder } = req.query;
         const result = await getOrders({ idUser, state, idOrder });
         return res.status(200).json({
             status: 'success',
