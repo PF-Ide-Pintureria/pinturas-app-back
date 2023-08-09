c62cc394aef9936a3414c6b5e58c0191fba62589
Jamer Rebolledo <jamerrq@gmail.com>
2023-08-08 23:20:32 -0500

    [HOTFIX] rounded rating with Math.floor

--- src/controllers/Reviews/createReviewController.js
+++ src/controllers/Reviews/createReviewController.js
@@ -9,11 +9,16 @@ const createReviewController = async ({ orderId,
     if (!order) throw Error('ORDEN NO ENCONTRADA');
 
     const review = await order.getReview();
-    if (review) {
-        throw new Error('ESTA ORDEN YA TIENE UNA REVIEW');
-    }
+    // if (review) {
+    //     throw new Error('ESTA ORDEN YA TIENE UNA REVIEW');
+    // }
 
     const newReview = await Reviews.create({ description, rating });
+
+    // console.log({
+    //     orderId,
+    //     description, rating, productsReviews
+    // });
 
 
     // Actualizamos el rating de los productos
@@ -21,13 +26,26 @@ const createReviewController = async ({ orderId,
         const parsedProductReview = JSON.parse(productReview);
         const { id, rating } = parsedProductReview;
         const product = await Products.findByPk(id);
-        const productRating = product.rating;
+        const productRating = product.rating || 0;
         const productRatingCount = product.nroReviews;
-        const newProductRating = (productRating * productRatingCount + rating) / (productRatingCount + 1);
-        await product.update({ rating: newProductRating, nroReviews: productRatingCount + 1 });
+        const num = (parseInt(productRating) * parseInt(productRatingCount) + parseInt(rating));
+        const den = (productRatingCount + 1);
+        const newProductRating = num / den;
+        const roundRating = Math.floor(newProductRating);
+        // if(roundRating > 5) throw new Error('El rating no puede ser mayor a
+        // 5');
+        // console.log(`Este producto tiene ${productRatingCount} reviews`);
+        // console.log(`El rating del producto es ${productRating}`);
+        // console.log(`El nuevo rating del producto es ${newProductRating}`);
+        // console.log(`El nuevo rating del producto redondeado es ${roundRating}`);
+        // console.log('roundRating', roundRating);
+        await product.update({
+            rating: roundRating,
+            nroReviews: productRatingCount + 1
+        });
     }));
 
-    await order.setReview(newReview);
+    // await order.setReview(newReview);
 
     return await Reviews.findByPk(newReview.id);
 };
