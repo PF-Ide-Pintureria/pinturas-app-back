a55e32a179ca61f7038bf9da57c66b615358df91
Edgar Juan Guercio <jguercio@live.com.ar>
2023-08-09 15:27:02 -0300

    [HOTFIX] cors

--- src/app.js
+++ src/app.js
@@ -34,8 +34,8 @@ server.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods, Authorization',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Methods',
         'GET, POST, OPTIONS, PUT, DELETE');
     next();
 });
