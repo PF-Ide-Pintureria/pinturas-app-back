9c500fd1219ecd43103de41bc9c9842c16031fd4
Edgar Juan Guercio <jguercio@live.com.ar>
2023-09-01 00:32:13 -0300

    funcionalidad para cargar una lista de excel y actualizar el precio de los productos de la DB

--- src/controllers/Products/updatePricesController.js
+++ src/controllers/Products/updatePricesController.js
@@ -1,7 +1,43 @@
+const xlsx = require('xlsx');
+const { Products } = require('../../db.js');
 
+const updatePricesController = async (excelFile) => {
+    const productsDb = await Products.findAll();
+    const productsIndex = {};
 
-const updatePricesController = () => {
-    return "controlador actualizar precios";
+    productsDb.forEach(product => {
+        productsIndex[product.code] = product;
+    });
+    // Leer y procesar el archivo Excel
+    const workbook = xlsx.read(excelFile.buffer); // Leer el archivo en memoria
+
+    // Obtener la primera hoja del libro
+    const firstSheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[firstSheetName];
+
+    // Convertir los datos en formato JSON
+    const productsFromExcel = xlsx.utils.sheet_to_json(worksheet);
+
+    let updateds = 0;
+
+    for await (const productExcel of productsFromExcel) {
+        const code = productExcel.codigo;
+        const newPrice = productExcel.precio;
+
+        if (productsIndex[code]) {
+            // Encontrar el producto en la DB por código
+            const productDB = productsIndex[code];
+
+            // Actualizar el precio en la DB si es diferente
+            if (productDB.price !== newPrice) {
+                await productDB.update({ price: newPrice });
+                updateds = updateds + 1;
+            }
+        }
+    };
+
+    return `Se han actualizado ${updateds} productos`;
+
 };
 
 module.exports = updatePricesController;
--- src/handlers/Products/updatePricesHandler.js
+++ src/handlers/Products/updatePricesHandler.js
@@ -1,10 +1,15 @@
 const { ProductsControllers } = require('../../controllers');
 const { updatePrices } = ProductsControllers;
 
-const updatePricesHandler = (req, res) => {
+const updatePricesHandler = async (req, res) => {
+    const uploadedFile = req.file;
 
-    const result = updatePrices();
-    res.send(result);
+    // Verificar si se cargó un archivo
+    if (!uploadedFile) return res.status(400).json({ error: 'No se cargó ningún archivo.' });
+
+
+    const result = await updatePrices(uploadedFile);
+    res.status(200).send(result);
 };
 
 module.exports = updatePricesHandler;
